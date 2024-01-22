2a55bda021563ee94769e14adc9d8da60f5f8f3f
babaHedz <hedi.rivas.pro@gmail.com>
2024-01-22 13:44:40 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const db = require('./database/setup'); // Do not edit/remove this line
+
+
+// Create todo with name and priority (done = false by default)
+function createTodo(name, priority) {}
+
+
+// Complete one todo by name (update property done = true)
+function completeTodo(name) {}
+
+
+// Delete one todo by name
+function deleteTodo(name) {}
+
+
+module.exports = { createTodo, completeTodo, deleteTodo }; // Do not edit/remove this line
---
+++ connection.js
@@ -0,0 +1,4 @@
+// Insert your connection string inside this variable
+const connectionString = 'mongodb+srv://.../playwithmongoose';
+
+module.exports = connectionString; // Do not edit/remove this line
